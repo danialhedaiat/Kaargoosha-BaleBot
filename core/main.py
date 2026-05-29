76a9299ea9e514e7d6aed2b7f116d8f0615a8244
dan <danialhedaiat@gmail.com>
2026-05-30 01:16:48 +0330

    [update] - phase one of sign up and sign in is done

--- core/main.py
+++ core/main.py
@@ -3,7 +3,8 @@ import re
 import json
 import traceback
 
-from telegram import BotCommand, Update, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+from telegram import BotCommand, Update, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, \
+    ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 import os
 
@@ -33,6 +34,7 @@ class BaleBot():
         self.app.add_handler(CommandHandler("start", self.start_command))
         self.app.add_handler(CommandHandler("join", self.join_command))
 
+        self.app.add_handler(CallbackQueryHandler(self.sign_in_command, pattern="sign_in"))
         self.app.add_handler(CallbackQueryHandler(self.sign_up_command, pattern="sign_up"))
         self.app.add_handler(CallbackQueryHandler(self.join_command, pattern="join"))
         self.app.add_handler(CallbackQueryHandler(self.create_command, pattern="create"))
@@ -48,11 +50,10 @@ class BaleBot():
     async def start_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         try:
             context.user_data["username"] = update.effective_user.username
-            context.user_data["phone_number_status"] = None
-            context.user_data["loading"] = True
+            context.user_data["phone_number_create_user_flag"] = None
+            context.user_data["phone_number_join_user_flag"] = None
             context.user_data["firstname_flag"] = None
             context.user_data["lastname_flag"] = None
-
 
             keyboard = [
                 [InlineKeyboardButton("ورود به حساب کاربری", callback_data="sign_in")],
@@ -61,7 +62,7 @@ class BaleBot():
 
             reply_markup = InlineKeyboardMarkup(keyboard)
             await update.effective_message.reply_text(
-                "به ربات کارگشا خوش امدید",
+                "به ربات کارگشا خوش امدید\nاگر قبلا در پلتفرم های دیگه ما ثبت نام کرده‌اید کافیست از منو زیر ثبت نام در بله رو انتخاب کنید",
                 reply_markup=reply_markup
             )
 
@@ -69,9 +70,38 @@ class BaleBot():
             logger.error(e)
 
     async def sign_in_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        pass
+        context.user_data["flow"] = "sign_in"
+        try:
+            self.publisher.get_user_by_username(body=context.user_data,
+                                            callback=self.after_sign_in,
+                                            callback_kwargs={"update": update, "context": context})
+
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            logger.error(e)
+
+    async def after_sign_in(self, update: Update, context: ContextTypes.DEFAULT_TYPE, response):
+        try:
+
+            if "error" in response:
+                keyboard = [
+                    [InlineKeyboardButton("بازگشت به شروع", callback_data="start")],
+                ]
+
+                reply_markup = InlineKeyboardMarkup(keyboard)
+                await update.effective_message.reply_text("اکانت شما یافت نشد",
+                                                          reply_markup=reply_markup)
+                return
+            message = "به حساب کاربری خود خوش امدید\nحساب کاربری شما:\n" + "\n".join(
+                f"{field}: {response[field]}" for field in response
+            )
+            await update.effective_message.reply_text(message)
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            logger.error(e)
 
     async def sign_up_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        context.user_data["flow"] = "sign_up"
         try:
             keyboard = [
                 [InlineKeyboardButton("با پلتفرم دیگری ثبت نام کردم", callback_data="join")],
@@ -84,10 +114,12 @@ class BaleBot():
                 reply_markup=reply_markup
             )
         except Exception as e:
+            logger.error(traceback.format_exc())
             logger.error(e)
 
     async def create_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         user = update.callback_query.from_user
+        context.user_data["flow"] = "create"
         if user.is_bot:
             return
         try:
@@ -103,26 +135,33 @@ class BaleBot():
                 "لطفاً شماره خود را ارسال کنید:",
                 reply_markup=reply_markup
             )
-            context.user_data["phone_number_status"] = True
+            context.user_data["phone_number_create_user_flag"] = True
         except Exception as e:
             logger.error(e)
 
     async def contact_listener(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         try:
-            if not context.user_data["phone_number_status"]:
-                return
             phone = update.message.contact.phone_number
 
             context.user_data["phone_number"] = "0" + phone[-10:]
+            if context.user_data["phone_number_create_user_flag"] and context.user_data["flow"] == "create":
 
-            self.publisher.user_phone_number_check(body=context.user_data, callback=self.check_phone_number_create_user_handler,
-                                                   callback_kwargs={"update": update, "context":context})
+                self.publisher.user_phone_number_check(body=context.user_data,
+                                                       callback=self.check_phone_number_create_user_handler,
+                                                       callback_kwargs={"update": update, "context": context})
+
+            elif context.user_data["phone_number_join_user_flag"] or context.user_data["flow"] == "join":
+
+                self.publisher.user_phone_number_check(body=context.user_data,
+                                                       callback=self.check_phone_number_join_user_handler,
+                                                       callback_kwargs={"update": update, "context": context})
         except Exception as e:
             logger.error(traceback.format_exc())
             logger.error(e)
 
     async def text_message_listener(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        if context.user_data["phone_number_status"]:
+
+        if context.user_data["phone_number_create_user_flag"] and context.user_data["flow"] == " create":
             phone = update.message.text
             context.user_data["phone_number"] = "0" + phone[-10:]
 
@@ -130,27 +169,46 @@ class BaleBot():
                 await update.effective_message.reply_text(
                     "لطفاً شماره معتبر وارد کنید یا از دکمه ارسال شماره استفاده کنید 📱",
                 )
-            self.publisher.user_phone_number_check(body=context.user_data, callback=self.check_phone_number_create_user_handler,
-                                                   callback_kwargs={"update": update, "context":context})
-            context.user_data["phone_number_status"] = False
+
+            self.publisher.user_phone_number_check(body=context.user_data,
+                                                   callback=self.check_phone_number_create_user_handler,
+                                                   callback_kwargs={"update": update, "context": context})
+            context.user_data["phone_number_create_user_flag"] = False
+
+        elif context.user_data["phone_number_join_user_flag"] or context.user_data["flow"] == "join":
+            phone = update.message.text
+            context.user_data["phone_number"] = "0" + phone[-10:]
+
+            if not re.fullmatch(r"\+?\d{11}", context.user_data["phone_number"]):
+                await update.effective_message.reply_text(
+                    "لطفاً شماره معتبر وارد کنید یا از دکمه ارسال شماره استفاده کنید 📱",
+                )
+
+            self.publisher.user_phone_number_check(body=context.user_data,
+                                                   callback=self.check_phone_number_join_user_handler,
+                                                   callback_kwargs={"update": update, "context": context})
+
         elif context.user_data["firstname_flag"]:
             context.user_data["first_name"] = update.message.text
             context.user_data["firstname_flag"] = False
             context.user_data["lastname_flag"] = True
 
             await update.message.reply_text("لطفا نام خانوادگی خود را وارد کنید ⬇️")
+
         elif context.user_data["lastname_flag"]:
             context.user_data["last_name"] = update.message.text
             context.user_data["lastname_flag"] = False
             self.publisher.user_create(body=context.user_data, callback=self.user_created,
-                                               callback_kwargs={"update": update, "context": context})
+                                       callback_kwargs={"update": update, "context": context})
 
-    async def check_phone_number_create_user_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE, message):
-        context.user_data["loading"] = False
-        if not message == "User does not exist":
+    async def check_phone_number_create_user_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE, response):
 
+        msg = await update.message.reply_text("درحال چک کردن شماره تلفن شما", reply_markup=ReplyKeyboardRemove())
+
+
+        if "error" in response and not response["error"] == "User does not exist":
             keyboard = [
-                [InlineKeyboardButton("بازگشت", callback_data="start")],
+                [InlineKeyboardButton("بازگشت به شروع", callback_data="start")],
             ]
 
             reply_markup = InlineKeyboardMarkup(keyboard)
@@ -160,14 +218,16 @@ class BaleBot():
 
         context.user_data["firstname_flag"] = True
 
-        await update.message.reply_text("لطفا نام کوچک خود را وارد کنید ⬇️")
+        await update.effective_message.reply_text(
+            "لطفا نام کوچک خود را وارد کنید ⬇️",
+            reply_markup=ReplyKeyboardRemove()
+        )
 
-        context.user_data["phone_number_status"] = False
+        context.user_data["phone_number_create_user_flag"] = False
 
-    async def user_created(self, update: Update, context: ContextTypes.DEFAULT_TYPE, user):
-        print(user)
+    async def user_created(self, update: Update, context: ContextTypes.DEFAULT_TYPE, response):
         try:
-            if "message" in user:
+            if "error" in response:
                 keyboard = [
                     [InlineKeyboardButton("بازگشت", callback_data="start")],
                 ]
@@ -177,9 +237,8 @@ class BaleBot():
                                                           reply_markup=reply_markup)
                 return
 
-            user = json.loads(user)
             message = "user created:\n" + "\n".join(
-                f"{field}: {user[field]}" for field in user
+                f"{field}: {response[field]}" for field in response
             )
             await update.message.reply_text(message)
         except Exception as e:
@@ -191,6 +250,7 @@ class BaleBot():
         if user.is_bot:
             return
         try:
+            context.user_data["sign_in_status"] = "join"
             query = update.callback_query
             await query.answer()
             keyboard = [
@@ -204,20 +264,48 @@ class BaleBot():
                 reply_markup=reply_markup
             )
 
-
-            context.user_data["phone_number_status"] = True
+            context.user_data["phone_number_join_user_flag"] = True
 
         except Exception as e:
             logger.error(e)
 
-    async def check_phone_number_join_user_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE, message):
-        context.user_data["loading"] = False
+    async def check_phone_number_join_user_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE, response):
 
-        context.user_data["firstname_flag"] = True
+        msg = await update.message.reply_text("درحال چک کردن شماره تلفن شما", reply_markup=ReplyKeyboardRemove())
 
-        await update.message.reply_text("لطفا نام کوچک خود را وارد کنید ⬇️")
+        if "error" in response and response["error"] == "User does not exist":
+            keyboard = [
+                [InlineKeyboardButton("بازگشت به شروع", callback_data="start")],
+            ]
 
-        context.user_data["phone_number_status"] = False
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            await update.effective_message.reply_text(
+                "حساب با این شماره پیدا ساخته نشده است.\nگزینه بازگشت را زده و حساب جدید برای خودتان ایجاد کنید",
+                reply_markup=reply_markup)
+            return
+
+        self.publisher.user_join_from_different_platform(body=context.user_data, callback=self.user_joined,
+                                                         callback_kwargs={"update": update, "context": context})
+
+    async def user_joined(self, update: Update, context: ContextTypes.DEFAULT_TYPE, response):
+        try:
+            if "error" in response:
+                keyboard = [
+                    [InlineKeyboardButton("بازگشت به شروع", callback_data="start")],
+                ]
+
+                reply_markup = InlineKeyboardMarkup(keyboard)
+                await update.effective_message.reply_text("شما در این پلتفرم با شماره تلفن همراه دیگری حساب ساخته اید.",
+                                                          reply_markup=reply_markup)
+                return
+
+            message = "اکانت بله شما با موفقیت به حساب کاربری شما متصل گردید\nحساب کاربری شما:\n" + "\n".join(
+                f"{field}: {response[field]}" for field in response
+            )
+            await update.message.reply_text(message)
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            logger.error(e)
 
 
 if __name__ == "__main__":
